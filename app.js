736a9da008638982db18afbb343276af9107dae0
Pawan deore <pavandeore90@gmail.com>
2021-02-04 16:59:35 +0530

    tayar

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ let btn = document.getElementById('btn');
 
 btn.addEventListener("click",()=>{
   let no = document.getElementById('no').value;
-  let link = "https://wa.me/"+no;
+  let link = "https://wa.me/91"+no;
     window.open(link);
 })
