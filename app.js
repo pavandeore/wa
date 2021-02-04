b88059516edec73ea7442886109fae0d66813664
Pawan deore <pavandeore90@gmail.com>
2021-02-04 16:47:23 +0530

    tayar

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let no = document.getElementById('no').value;
 let btn = document.getElementById('btn');
 
 btn.addEventListener("click",()=>{
-  let link = `https://api.whatsapp.com/send?phone=${no}`;
+  let link = `https://api.whatsapp.com/send?phone=91${no}`;
     window.open(link);
+    no = "";
 })
